08215bcaee7a61a4133bc6206cd4d2581c3d6ea1
Geno Roupsky <geno@roupsky.name>
2013-11-05 13:58:32 +0200

    fixed installer check and moved android stuff to linux only

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ function get() {
 }
 
 function check() {
-	return command -v "$1" >/dev/null 2>&1
+	command -v "$1" >/dev/null 2>&1
 }
 
 if [ "$(uname)" == "Darwin" ]; then
@@ -27,13 +27,16 @@ if [ "$(uname)" == "Darwin" ]; then
     brew install bash-completion
     
     # install hub
-    brew install hub
+    check hub || brew install hub
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Linux detected"
     
     # install hub
-    curl http://hub.github.com/standalone -sLo ~/bin/hub
+    check hub || curl http://hub.github.com/standalone -sLo ~/bin/hub
     chmod +x ~/bin/hub
+
+    wget -qO- https://gist.github.com/groupsky/3747983/raw/1475fe951de103397357445af6f7b4aa4e5cb505/51-android.rules | sudo tee /etc/udev/rules.d/51-android.rules > /dev/null
+    wget -qO- https://gist.github.com/groupsky/3747983/raw/73bebae7b15cdf0477c45a92b0f18ad17f8f28ba/adb_usb.ini > ~/.android/adb_usb.ini
 
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "Windows NT detected"
@@ -44,6 +47,4 @@ get .gitconfig
 get .bashrc
 get .profile
 get .bash_aliases
-wget -qO- https://gist.github.com/groupsky/3747983/raw/1475fe951de103397357445af6f7b4aa4e5cb505/51-android.rules | sudo tee /etc/udev/rules.d/51-android.rules > /dev/null
-wget -qO- https://gist.github.com/groupsky/3747983/raw/73bebae7b15cdf0477c45a92b0f18ad17f8f28ba/adb_usb.ini > ~/.android/adb_usb.ini
 

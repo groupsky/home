34bb212fc8279ed944fe671843b967e27c8bc312
Geno Roupsky <geno@roupsky.name>
2014-02-12 18:16:35 +0200

    added libyaml-dev curl

--- install.sh
+++ install.sh
@@ -49,14 +49,15 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Linux detected"
 
     sudo aptitude update
-    sudo aptitude install git mc python-setuptools python-dev compizconfig-settings-manager compiz-plugins build-essential
+    sudo aptitude install git mc python-setuptools python-dev compizconfig-settings-manager compiz-plugins build-essential libyaml-dev curl
     
     # install hub
     check hub || curl http://hub.github.com/standalone -sLo ~/bin/hub
     chmod +x ~/bin/hub
 
+    mkdir -p $HOME/.android
     wget -qO- https://gist.github.com/groupsky/3747983/raw/1475fe951de103397357445af6f7b4aa4e5cb505/51-android.rules | sudo tee /etc/udev/rules.d/51-android.rules > /dev/null
-    wget -qO- https://gist.github.com/groupsky/3747983/raw/73bebae7b15cdf0477c45a92b0f18ad17f8f28ba/adb_usb.ini > ~/.android/adb_usb.ini
+    wget -qO- https://gist.github.com/groupsky/3747983/raw/73bebae7b15cdf0477c45a92b0f18ad17f8f28ba/adb_usb.ini > $HOME/.android/adb_usb.ini
 
 
 
